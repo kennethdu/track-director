2c709acae6803d0545e8760c9e3a0aad9896b956
Kenneth Du <kennethdu3@gmail.com>
2018-08-10 13:20:37 -0700

    add enter name form in index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,16 @@ $(document).ready(function(){
     $("button#btn").click(function() {
 
       var add = function(answer, answer2, answer3, answer4, answer5) {
-        return answer + answer2 + answer3 + answer4 + answer5;
-};
-
+      return answer + answer2 + answer3 + answer4 + answer5;
+	};
+		var person1 = $(input.person1).val();
 		var answer = parseInt($("input:radio[name='question1']:checked").val());
 		var answer2 = parseInt($("input:radio[name='question2']:checked").val());
 		var answer3 = parseInt($("input:radio[name='question3']:checked").val());
 		var answer4 = parseInt($("input:radio[name='question4']:checked").val());
 		var answer5 = parseInt($("input:radio[name='question5']:checked").val());
 		var result = parseInt(add(answer, answer2, answer3, answer4, answer5).toString());
-      
-	  
-	  
+
 		if (result===5 || result==6 || result==7) {
 		alert("YOU SHOULD STUDY React");
 		$('#react').show()
@@ -32,10 +30,8 @@ $(document).ready(function(){
 
 		}
 
-
-
-      $("#output").text(result);
-
+    $("#output").text(result);
+	$(".person1").text(person1);
 
 
 });});
